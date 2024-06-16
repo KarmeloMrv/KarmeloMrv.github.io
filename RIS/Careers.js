b1e4f1f159aec15fdaca9077e7be0e55f4757665
Karmelo Mrvica <126247560+KarmeloMrv@users.noreply.github.com>
2024-06-16 16:00:02 +0200

    Delete RIS directory
    Retrying without the master folder

--- RIS/Careers.js
+++
@@ -1,8 +0,0 @@
-function showDetails(jobId) {
-    var details = document.getElementById(jobId + '-details');
-    if (details.style.display === 'none') {
-      details.style.display = 'block';
-    } else {
-      details.style.display = 'none';
-    }
-  }
